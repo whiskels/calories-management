f1112e786f9bda74f91f4c3cdf4174ce5681e60b
whiskels <kuzmingeo@gmail.com>
2020-07-11 14:03:26 +0300

    HW03 init meal DB

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +10,10 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, description, calories, date_time) VALUES
+(100000, 'Pastrami sandwich', 500, '2020-07-11 10:00:00'),
+(100000, 'Orange juice', 100, '2020-07-11 12:00:00'),
+(100000, 'Vegetable salad', 500, '2020-07-11 14:00:00'),
+(100000, 'Ice cream', 500, '2020-07-11 20:00:00'),
+(100001, 'Admin super meal', 9999, current_timestamp);
