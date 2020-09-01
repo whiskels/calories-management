af4a06c702c83b1462c0d659e34281f9c7941e29
whiskels <kuzmingeo@gmail.com>
2020-09-01 19:20:11 +0300

    7 13 jackson object mapper

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -2,18 +2,14 @@ package ru.javawebinar.topjava.model;
 
 import org.springframework.util.Assert;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 
-import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
-import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
-
 @MappedSuperclass
 // http://stackoverflow.com/questions/594597/hibernate-annotations-which-is-better-field-or-property-access
 @Access(AccessType.FIELD)
-@JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, isGetterVisibility = NONE, setterVisibility = NONE)
+//@JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, isGetterVisibility = NONE, setterVisibility = NONE)
 public abstract class AbstractBaseEntity {
     public static final int START_SEQ = 100000;
 
